ba1dfb76182592334a902a917b86718fe22de845
lambc <lambc@enterprisenao.com>
2023-03-30 10:13:56 +0700

    abc

--- src/app/components/admin/admin.module.ts
+++ src/app/components/admin/admin.module.ts
@@ -22,9 +22,9 @@ import { ViewSupplierComponent } from './supplier/view-supplier/view-supplier.co
 import { DetailSupplierComponent } from './supplier/detail-supplier/detail-supplier.component';
 import { AddSupplierComponent } from './supplier/add-supplier/add-supplier.component';
 import { EditSupplierComponent } from './supplier/edit-supplier/edit-supplier.component';
-import { ProductComponent } from './product/product/product.component';
 import { CreateProductComponent } from './product/create-product/create-product.component';
 import { ViewProductComponent } from './product/view-product/view-product.component';
+import { ProductComponent } from './product/product.component';
 @NgModule({
   declarations: [
     AdminComponent,
